be6b4e54166589c72e598e88ac37c4895b189827
Mido N Nassar <madyan.nassar@gmail.com>
2021-04-20 20:10:51 +0200

    NodeJs homework Week 2

--- week1/homework/HackYourTemperature/server.js
+++ week1/homework/HackYourTemperature/server.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const server = express();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const axios = require("axios");
 
-server.get('/', (req, res) => {
-    res.send('hello from backend to frontend!');
-  });
-  
-  server.listen(3000);
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: false }));
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
+
+app.listen(PORT , ()=> console.log(`running on port: ${PORT}`));
--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,58 @@
 const express = require('express')
+const fs = require('fs');
 const app = express();
  
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+
+// Reading post
+app.get('/blogs/:title', (req, res) => {
+    const title = req.params.title;
+    if (fs.existsSync(title)) {
+      const post = fs.readFileSync(title);
+      res.end(post);
+    } else {
+      res.status(400).end({msg: "No content , please check the title"});
+    }
+  });
+
+  // Creating new post
+  app.post('/blogs', (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    if (!title || !content) {
+      return res.status(400).send({ msg: "can not create post without title or content " });
+    }
+    fs.writeFileSync(title, content);
+    res.end({ msg : 'Done!! , the post is already created'});
+  });
+
+  // Updating post
+  app.put('/posts/:title', (req, res) => {
+    const title = req.params.title;
+    const content = req.body.content;
+    if (fs.existsSync(title)) {
+      fs.writeFileSync(title, content);
+      res.end('Done!! , the post is already updated');
+    } else {
+      res.status(400);
+      res.send({ msg: 'There is no post to update with this title, please check the title!' });
+    }
+  });
+
+  //  Deleting post
+  app.delete('/blogs/:title', (req, res) => {
+    const title = req.params.title;
+    if (fs.existsSync(title)) {
+      fs.unlinkSync(title);
+      res.end('Done!! , the post is already deleted');
+    } else {
+      res.status(400).
+      res.send({ msg: 'There is no post to delete with this title, please check the title!' });
+    }
+  });
+  
+
+  app.listen(3000 , ()=> console.log('Start listening')); 
